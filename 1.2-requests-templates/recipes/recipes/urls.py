7b01dcacaa3d48f7f763e196ecd74f421d6a11be
Grachev Maksim <magra85@gmail.com>
2022-06-25 22:19:32 +0300

    Added HW 1.2

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 
 from django.urls import path
+from calculator.views import calculate_view
 
 urlpatterns = [
+    path('<dish_get>/', calculate_view, name='calculate')
     # здесь зарегистрируйте вашу view-функцию
 ]
