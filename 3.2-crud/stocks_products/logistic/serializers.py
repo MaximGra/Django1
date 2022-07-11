c8cfa16523727ee01256ad44737542180a660e64
Grachev Maksim <magra85@gmail.com>
2022-07-12 02:23:37 +0300

    Update

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,62 +1,40 @@
 from rest_framework import serializers
-from .models import Product, Stock, StockProduct
+
+from logistic.models import Product, StockProduct, Stock
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
     class Meta:
         model = StockProduct
         fields = ['product', 'quantity', 'price']
+    # настройте сериализатор для позиции продукта на складе
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
     class Meta:
         model = Stock
-        fields = '__all__'
+        fields = ['address', 'positions']
+
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
-
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-        for element in positions:
-            StockProduct.objects.create(stock=stock, **element)
-
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock, **position)
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
+        stock = instance
         stock = super().update(instance, validated_data)
-
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-        for element in positions:
-            objects, created = StockProduct.objects.update_or_create(
-                stock=stock,
-                product=element['product'],
-                defaults={'stock': stock,
-                'product': element['product'],
-                'quantity': element['quantity'],
-                'price': element['price']
-                }
-            )
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock, **position)
 
         return stock
--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -41,7 +41,16 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'logistic',
+    'django_filters',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
